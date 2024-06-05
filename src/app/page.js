e72f0a91ea74deca55465307d2e0c8e198eed859
Abhay Hemant Upadhaya <abhayupadhaya829@gmail.com>
2024-06-05 19:54:46 +0530

    Done

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,272 @@
-import Image from "next/image";
+'use client'
+
+import PrivateRoute from "@/component/PrivateRoute";
+import {db} from '../config'
+import { collection,addDoc,getDocs,deleteDoc,serverTimestamp,query,orderBy,doc,updateDoc } from "firebase/firestore";
+import React,{useState,useEffect} from "react";
+import {toast} from 'react-hot-toast'
+import { useAuthContext } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+
+
+
+async function addTodo(title,details,dueDate){
+  try {
+    const docRef = await addDoc(collection(db,"todos"),{
+      title:title,
+      details:details,
+      dueDate:dueDate,
+      createdAt:serverTimestamp()
+    })
+    console.log("Todo added with ID:",docRef.id)
+    return true
+  } catch (error) {
+    console.log("Error adding todo:",error);
+    return false;
+  }
+}
+
+async function fetchTodo(){
+  const todosCollection = collection(db,"todos")
+  const querySnapshot = await getDocs(query(todosCollection,orderBy("createdAt","desc")))
+  const todos = [];
+  querySnapshot.forEach((doc)=>{
+    const todoData = doc.data();
+    todos.push({id:doc.id,...todoData})
+  })
+  return todos;
+}
+
+async function deleteTodo(todoId){
+  try {
+    console.log('Attempting to delete todo with ID:',todoId);
+    await deleteDoc(doc(db,"todos",todoId))
+    return todoId;
+  } catch (error) {
+    console.error("Error in deleting the todo:",error)
+    return null;
+  }
+}
 
 export default function Home() {
+  const { user, logout } = useAuthContext();
+  const router = useRouter();
+
+  const [title,setTitle] = useState("");
+  const [details,setDetails] = useState("");
+  const [dueDate,setDueDate] = useState("");
+  const [todos,setTodos] = useState([]);
+  const [selectedTodo,setSelectedTodo]=useState(null);
+  const [isUpdateMode,setIsUpdateMode] = useState(false);
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault(); 
+    if(isUpdateMode){
+      if(selectedTodo){
+        try {
+          const updateTodo = {
+            title,
+            details,
+            dueDate
+          }
+          const todoRef = doc(db,"todos",selectedTodo.id);
+          await updateDoc(todoRef,updateTodo);
+
+          setTodos(todos.map(todo => (todo.id === selectedTodo.id ? { ...todo, ...updateTodo } : todo)));
+
+          setTitle("");
+          setDetails("");
+          setDueDate("");
+          setSelectedTodo(null);
+          setIsUpdateMode(false);
+          toast.success("Updated Successfully !")
+
+        } catch (error) {
+          console.error("Error updating todo :",error)
+        }
+      }
+    }else{
+      const added = await addTodo(title,details,dueDate);
+      if(added){
+        const updatedTodos = await fetchTodo();
+      setTodos(updatedTodos);
+
+        setTitle("");
+        setDetails("");
+        setDueDate("");
+        toast.success("Todo added successfully !")
+        
+      }
+    }
+  }
+
+  useEffect(()=>{
+    async function fetchTodos(){
+      const todos = await fetchTodo();
+      setTodos(todos);
+    }
+    fetchTodos();
+  },[])
+
+
+  const handleUpdateClick = (todo)=>{
+    setTitle(todo.title || "");
+    setDetails(todo.details || "");
+    setDueDate(todo.dueDate || "");
+
+    setSelectedTodo(todo);
+    setIsUpdateMode(true);
+  }
+
+
+  useEffect(()=>{
+    async function fetchTodos(){
+      const todos = await fetchTodo();
+      setTodos(todos);
+    }
+    fetchTodos();
+  },[])
+
+  const handleDelete = async(todo)=>{
+    const deleteTodoId = await deleteTodo(todo.id)
+    if(deleteTodoId){
+      const updateTodos = todos.filter((t)=> t.id !== deleteTodoId)
+      setTodos(updateTodos)
+    }
+  }
+
+  const handleLogout = async () => {
+    try {
+        await logout();
+        router.push("/");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <PrivateRoute>
+    <main className="flex flex-1 items-center justify-center flex-col md:flex-row min-h-screen bg-gradient-to-r from-indigo-500 to-purple-500">
+    <button
+          className="absolute top-4 right-4 px-4 py-2 text-sm font-semibold text-white bg-red-500 hover:bg-red-600 rounded-md transition duration-300 focus:outline-none focus:ring-2 focus:ring-red-500"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      <section className="flex-1 flex md:flex-col items-center md:justify-start mx-auto">
+        <div className="p-6 md:p-12 mt-10 rounded-lg shadow-2xl w-full max-w-lg bg-white">
+          <h2 className="text-center text-3xl font-bold leading-9 text-gray-900 mb-6">
+            {isUpdateMode ? "Update Your Todo" : "Create a Todo"}
+            
+          </h2>
+          <form className="mt-6 space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="title" className="block text-base font-medium leading-6 text-gray-700">
+                Title
+              </label>
+              <div className="mt-2">
+                <input
+                  id="title"
+                  name="title"
+                  type="text"
+                  autoComplete="off"
+                  required
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  className="w-full rounded-md border border-gray-300 py-2 pl-2 text-gray-900 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                />
+              </div>
+            </div>
+  
+            <div>
+              <label htmlFor="details" className="block text-base font-medium leading-6 text-gray-700">
+                Details
+              </label>
+              <div className="mt-2">
+                <textarea
+                  id="details"
+                  name="details"
+                  rows="4"
+                  autoComplete="off"
+                  required
+                  value={details}
+                  onChange={(e) => setDetails(e.target.value)}
+                  className="w-full rounded-md border border-gray-300 py-2 pl-2 text-gray-900 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="dueDate" className="block text-base font-medium leading-6 text-gray-700">
+                Due Date
+              </label>
+              <div className="mt-2">
+                <input
+                  id="dueDate"
+                  name="dueDate"
+                  type="date"
+                  autoComplete="off"
+                  required
+                  value={dueDate}
+                  onChange={(e) => setDueDate(e.target.value)}
+                  className="w-full rounded-md border border-gray-300 py-2 pl-2 text-gray-900 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="w-full bg-indigo-600 text-white py-3 px-4 rounded-md font-semibold hover:bg-indigo-700 transition duration-300 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              >
+                {isUpdateMode ? "Update Todo" : "Create Todo"}
+              </button>
+            </div>
+          </form>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      </section>
+  
+      <section className="md:w-1/2 md:max-h-screen overflow-y-auto md:ml-20 mx-auto">
+        <div className="p-6 md:p-12 mt-10 rounded-lg shadow-2xl w-full max-w-lg bg-white">
+          <h2 className="text-center text-3xl font-bold leading-9 text-gray-900 mb-6">
+            Todo List
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <div className="mt-6 space-y-6">
+            {todos.map((todo) => (
+              <div
+                key={todo.id}
+                className="border border-gray-300 rounded-lg p-6 shadow-md bg-white hover:shadow-lg transition duration-300"
+              >
+                <h3 className="text-xl font-semibold text-gray-900 break-words">
+                  {todo.title}
+                </h3>
+                <p className="text-base text-gray-600 mt-2">
+                  Due Date: {todo.dueDate}
+                </p>
+                <p className="text-gray-700 multiline break-words mt-4">
+                  {todo.details}
+                </p>
+                <div className="mt-6 flex justify-end space-x-2">
+                  <button
+                    type="button"
+                    className="px-4 py-2 text-base font-semibold text-white bg-blue-500 hover:bg-blue-600 rounded-md transition duration-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    onClick={() => handleUpdateClick(todo)}
+                  >
+                    Update
+                  </button>
+                  <button
+                    type="button"
+                    className="px-4 py-2 text-base font-semibold text-white bg-red-500 hover:bg-red-700 rounded-md transition duration-300 focus:outline-none focus:ring-2 focus:ring-red-500"
+                    onClick={() => handleDelete(todo)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </main>
+  </PrivateRoute>
   );
 }
